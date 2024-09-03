8f420508bf4d4ce741e960f7a424f5ba0da5c762
Natzujj <gabrielnatsujake@gmail.com>
2024-09-03 13:33:44 -0300

    Logica Calculadora - Funcionamento de Operacoes

--- src/br/com/natzuj/calc/modelo/Memoria.java
+++ src/br/com/natzuj/calc/modelo/Memoria.java
@@ -46,10 +46,38 @@ public class Memoria {
         } else if (tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
             textoAtual = substituir ? texto : textoAtual + texto;
             substituir = false;
+        } else {
+            substituir = true;
+            textoAtual = obterResultadoOperacao();
+            textoBuffer = textoAtual;
+            ultimaOperacao = tipoComando;
         }
         
 
         observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+    }
+
+    private String obterResultadoOperacao() {
+        if (ultimaOperacao == null) {
+            return textoAtual;
+        }
+
+        double numeroBuffer = Double.parseDouble(textoBuffer.replace(",", "."));
+        double numeroAtual = Double.parseDouble(textoAtual.replace(",", "."));
+        double resultado = 0;
+
+        if (ultimaOperacao == TipoComando.SOMA) {
+            resultado = numeroBuffer + numeroAtual;
+        }else if(ultimaOperacao == TipoComando.SUBTRACAO){
+            resultado = numeroBuffer - numeroAtual;
+        }else if(ultimaOperacao == TipoComando.MULTIPLICACAO){
+            resultado = numeroBuffer * numeroAtual;
+        } else if (ultimaOperacao == TipoComando.DIVISAO) {
+            resultado = numeroBuffer - numeroAtual;
+        }
+        String resultadoString = Double.toString(resultado).replace(".", ",");
+        boolean inteiro = resultadoString.endsWith(",0");
+        return inteiro ? resultadoString.replace(",0", "") : resultadoString;
     }
 
     private TipoComando detectarTipoComando(String texto) {
