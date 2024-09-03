c37d3353426e831a144c6602bce4d9ce8185e85c
Natzujj <gabrielnatsujake@gmail.com>
2024-09-03 12:57:50 -0300

    Logica Calculadora - Tipo de Comandos

--- src/br/com/natzuj/calc/modelo/Memoria.java
+++ src/br/com/natzuj/calc/modelo/Memoria.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Memoria {
 
+    private enum TipoComando{
+        ZERAR, NUMERO, DIVISAO, MULTIPLICACAO, SUBTRACAO, SOMA, IGUAL, VIRGULA;
+    };
+
     private static final Memoria instancia = new Memoria();
     private String textoAtual = "";
     private final List<MemoriaObserver> observadores = new ArrayList<>();
@@ -25,13 +29,46 @@ public class Memoria {
         return textoAtual.isEmpty() ? "0" : textoAtual;
     }
 
-    public void processarComando(String valor) {
-        if ("AC".equals(valor)) {
+    public void processarComando(String texto) {
+        TipoComando tipoComando = detectarTipoComando(texto);
+        System.out.println(tipoComando);
+
+        if ("AC".equals(texto)) {
             textoAtual = "";
         } else {
-            textoAtual += valor;
+            textoAtual += texto;
         }
         observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+    }
+
+    private TipoComando detectarTipoComando(String texto) {
+        if (textoAtual.isEmpty() && texto == "0") {
+            return null;
+        }
+
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        } catch (NumberFormatException e) {
+            //Quando nao for numero..
+            if ("AC".equalsIgnoreCase(texto)) {
+                return TipoComando.ZERAR;
+            }else if ("/".equals(texto)) {
+                return TipoComando.DIVISAO;
+            }else if ("x".equalsIgnoreCase(texto)) {
+                return TipoComando.MULTIPLICACAO;
+            }else if ("+".equals(texto)) {
+                return TipoComando.SOMA;
+            }else if ("-".equals(texto)) {
+                return TipoComando.SUBTRACAO;
+            }else if ("=".equals(texto)) {
+                return TipoComando.IGUAL;
+            }else if (",".equals(texto)) {
+                return TipoComando.VIRGULA;
+            }        
+        }
+
+        return null;
     }
         
 }
